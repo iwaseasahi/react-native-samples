1f9ac523e73f10d7238273f6ce2e295f2c4602d5
岩瀬旭 <eb50113@yahoo.co.jp>
2021-07-24 09:55:38 +0900

    Change function name

--- clock-app/App.tsx
+++ clock-app/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Clock() {
+export default function App() {
   const [date, setDate] = useState(() => new Date());
 
   // 副作用とそのクリーンアップを 1 度だけ（マウント時とアンマウント時にのみ）実行したいという場合、空の配列 ([]) を第 2 引数として渡すことができます。
