4f145cba72da98c1500036bc6f57a0d0982f4e2e
岩瀬旭 <eb50113@yahoo.co.jp>
2021-07-24 09:55:08 +0900

    Add comment

--- clock-app/App.tsx
+++ clock-app/App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Clock() {
   const [date, setDate] = useState(() => new Date());
 
+  // 副作用とそのクリーンアップを 1 度だけ（マウント時とアンマウント時にのみ）実行したいという場合、空の配列 ([]) を第 2 引数として渡すことができます。
+  // https://ja.reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
   useEffect(() => {
     const timerID = setInterval(() => {
       setDate(new Date())
